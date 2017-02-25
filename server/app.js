26e53885a962fbdeca39d047b7580b5e6b6e5f97
Bill Martin <bill@billmartin.io>
2017-02-25 16:02:47 -0500

    Add basic validation to only store medals if all data is present in POST request

--- server/app.js
+++ server/app.js
@@ -12,50 +12,54 @@ app.use( express.static(__dirname + "/../client") );
 app.use( '/node_modules', express.static(__dirname + "/../node_modules") );
 
 // Get all sports
-app.get("/sports", (request, response) => {
+app.get("/sports", (req, res) => {
 	let sports = mongoUtil.sports();
 
 	sports.find().toArray((err, docs) => {
 		if(err) {
-			response.sendStatus(400);
+			res.sendStatus(400);
 		}
 
 		let sportNames = docs.map((sport) => sport.name)
-		response.json(sportNames);
+		res.json(sportNames);
 	});
 });
 
 // Get the medals for a particular sport
-app.get("/sports/:name", (request, response) => {
-	let sportName = request.params.name;
+app.get("/sports/:name", (req, res) => {
+	let sportName = req.params.name;
 	let sports    = mongoUtil.sports();
 	
 	sports.find({name: sportName}).limit(1).next((err, doc) => {
 		if(err) {
-			response.sendStatus(400);
+			res.sendStatus(400);
 		}
 
 		console.log("Sport doc: ", doc);
-		response.json(doc);
+		res.json(doc);
 
 	});
 });
 
 // Create new medal
-app.post("/sports/:name/medals", jsonParser, (request, response) => {
-	let sportName = request.params.name;
-	let newMedal  = request.body.medal;
+app.post("/sports/:name/medals", jsonParser, (req, res) => {
+	let sportName = req.params.name;
+	let newMedal  = req.body.medal || {};
+
+	if(!newMedal.division || !newMedal.year || !newMedal.country) {
+		res.sendStatus(400);
+	}
 
 	// Store medal in DB
 	let sports    = mongoUtil.sports();
 	let query     = { name: sportName };
 	let update    = { $push: { goldMedals: newMedal } };
 
-	sports.findOneAndUpdate(query, update, (err, res) => {
+	sports.findOneAndUpdate(query, update, (err, update_res) => {
 		if(err) {
-			response.sendStatus(400);
+			res.sendStatus(400);
 		}
-		response.sendStatus(201);
+		res.sendStatus(201);
 	});
 });
 
